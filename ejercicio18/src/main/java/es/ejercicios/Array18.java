167d07529a746afaee064632db6332d796e0c325
Juanma <Juanma888@gmail.com>
2024-11-13 22:42:22 +0100

    Primer commit. He pegado código desde otro proyecto no Maven y no Git.

---
+++ ejercicio18/src/main/java/es/ejercicios/Array18.java
@@ -0,0 +1,30 @@
+
+package es.ejercicios;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+// Enunciado ejercicio 18:
+// 18. Write a Java program to find the second smallest element in an array. 
+
+public class Array18{
+
+    public static void main(String[] args){
+        
+        ArrayList<Integer> numeros = new ArrayList<>();
+        
+        // Agregar números al ArrayList
+        numeros.add(5);
+        numeros.add(2);
+        numeros.add(8);
+        numeros.add(1);
+
+        // Los ordenos de menor a mayor
+        Collections.sort(numeros);
+
+        // Busco el índice 1, que será el segundo menor número del ArrayList
+        int resultado = numeros.get(1);
+        System.out.println(resultado);
+
+    }
+}
